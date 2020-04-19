81410712c31e4ad8bd2436cd51959c85259e7cd6
lisirgo <1179648931@qq.com>
2020-04-19 20:43:52 +0800

    配置路由响应数据

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const server = http.createServer((request,response)=>{
-    response.writeHead(200,{
-        'Content-Type':'text/html'
-    })
-    response.write('<input />');
-    response.end();
+    switch (request.url){
+        case '/':
+            response.write('hello~');
+            break;
+        case '/posts':
+            response.write('posts');
+            break;
+        case '/signup':
+            response.write('signup');
+            break;
+        default:
+            response.writeHead('404');
+            response.write('404');
+        break;
+    }
+
+    response.end()
 })
 
-server.listen(3000,()=>{
+server.listen(4200,()=>{
     console.log('服务已启动');
 })
